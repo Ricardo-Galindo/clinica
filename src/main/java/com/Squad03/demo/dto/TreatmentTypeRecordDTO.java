45acd2766f252bcaa8525f716e4a89205eb8b753
Talita Santos <iftalisantos10@gmail.com>
2024-11-12 12:58:43 -0300

    Refatorando DTOs para usar records

--- src/main/java/com/Squad03/demo/dto/TreatmentTypeRecordDTO.java
+++ src/main/java/com/Squad03/demo/dto/TreatmentTypeRecordDTO.java
@@ -2,42 +2,4 @@ package com.Squad03.demo.dto;
 
 import java.util.UUID;
 
-public class TreatmentTypeRecordDTO {
-
-    private UUID treatmentTypeId;
-    private UUID responsibleId;
-    private UUID studentId;
-
-    public UUID getTreatmentTypeId() {
-        return treatmentTypeId;
-    }
-
-    public void setTreatmentTypeId(UUID treatmentTypeId) {
-        this.treatmentTypeId = treatmentTypeId;
-    }
-
-    public UUID getResponsibleId() {
-        return responsibleId;
-    }
-
-    public void setResponsibleId(UUID responsibleId) {
-        this.responsibleId = responsibleId;
-    }
-
-    public UUID getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(UUID studentId) {
-        this.studentId = studentId;
-    }
-
-    @Override
-    public String toString() {
-        return "TreatmentTypeRecordDTO{" +
-                "treatmentTypeId=" + treatmentTypeId +
-                ", responsibleId=" + responsibleId +
-                ", studentId=" + studentId +
-                '}';
-    }
-}
+public record TreatmentTypeRecordDTO( UUID treatmentTypeId, UUID responsibleId, UUID studentId){}
